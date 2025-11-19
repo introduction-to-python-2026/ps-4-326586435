b7de32926ef2f6edabd1e8035c3d3b85c34e5f55
326586435 <nourali0307@gmail.com>
2025-11-19 23:50:55 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -8,20 +8,18 @@ def split_before_each_uppercases(formula):
            split_formula.append(formula[start:i])
            start = i
     split_formula.append(formula[start:])
-return split_formula
+    return split_formula
+
 
 
 def split_at_first_digit(formula):
     digit_index = None
-
     for i, ch in enumerate(formula):
         if ch.isdigit():
             digit_index = i
             break
-
     if digit_index is None:
         return formula, 1
-
     x = formula[:digit_index]
     y = int(formula[digit_index:])
     return x, y
