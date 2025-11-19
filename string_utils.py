5141c07314d3cf17a2e4a28efafac79c2fc59c86
326586435 <nourali0307@gmail.com>
2025-11-19 23:41:52 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -5,8 +5,8 @@ def split_before_each_uppercases(formula):
         return []
     for i in range(1, len(formula)):
         if formula[i].isupper():
-            split_formula.append(formula[start:i])
-            start = i
+           split_formula.append(formula[start:i])
+           start = i
     split_formula.append(formula[start:])
     return split_formula
 
@@ -17,12 +17,12 @@ def split_at_first_digit(formula):
     digit_index=None
     for t in range(len(formula)):
         if formula[t].isdigit():
-            digit_index = t
-            break
+           digit_index = t
+           break
         if digit_index == None:
-            x=formula
-            y=1
+           x=formula
+           y=1
         else:
-            y = int(formula[digit_index:])
-            x = formula[0:digit_index]
+           y = int(formula[digit_index:])
+           x = formula[0:digit_index]
         return x, y
