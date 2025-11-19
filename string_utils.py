a26d89f7202f3c82ca8c22c9e575f7cb9984f50d
326586435 <nourali0307@gmail.com>
2025-11-19 23:39:41 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -6,7 +6,7 @@ def split_before_each_uppercases(formula):
     for i in range(1, len(formula)):
         if formula[i].isupper():
             split_formula.append(formula[start:i])
-            start = 1
+            start = i
     split_formula.append(formula[start:])
     return split_formula
 
